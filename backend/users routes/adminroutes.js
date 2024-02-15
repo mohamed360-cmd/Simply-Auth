59b9e60c8415ce37352beac6c13a63169b51c776
Mohammed Abdiladif <mihabulle45a@gmail.com>
2024-02-15 05:52:47 +0300

    bug fix

--- backend/users routes/adminroutes.js
+++ backend/users routes/adminroutes.js
@@ -2,11 +2,12 @@ require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
 const {Adminmodel} = require("../Model/AdminModel")
+const {userModel} = require("../Model/UserSchema")
 const bcrypt = require("bcrypt")
-const jtw = require("jsonwebtoken")
- async function createAdmin(){
-    const password = ""
-    const userName = ""
+const jwt = require("jsonwebtoken")
+ async function createAdmin(Email,Password){
+    const password = Password
+    const userName = Email
     const salt = await bcrypt.genSalt(12)
     const hashedpassword = await bcrypt.hash(password,salt)
     const addAdmin = await Adminmodel.create({
@@ -15,6 +16,7 @@ const jtw = require("jsonwebtoken")
     })
     addAdmin.save()
 }
+//create an Admin
 //createAdmin()
 const checkIfEmailExist = async(email)=>{//function for checkin is the email exists
     try{
@@ -49,7 +51,7 @@ routes.post("/login",async(req,res)=>{
         if(emailCheckResult.status ){
             const passwordCheckStatus = await checkIfPassMatching(Password,emailCheckResult.data.Password)
             if(passwordCheckStatus){
-                const jwtToken = jtw.sign({Email : Email},process.env.SECRET_KEY)
+                const jwtToken = jwt.sign({Email : Email},process.env.SECRET_KEY)
                 res.status(201).json({
                     status :true,
                     jwtToken : jwtToken
@@ -69,5 +71,35 @@ routes.post("/login",async(req,res)=>{
     } catch (error) {
         console.log("Error in the /login route",error)
     }
+})
+const getAllUserslist = async()=>{
+    try {
+           const userList = await userModel.find()
+           return userList
+    } catch (error) {
+        console.log("error in the getAllUsersList")
+    }
+}
+routes.get("/getUsers",async(req,res)=>{
+try {
+    const jwtToken = req.headers.authorization.replace("Bearer","")
+    const jwtTokenVerificationResult =  jwt.verify(jwtToken,process.env.SECRET_KEY)
+    const emailChechkResult = await checkIfEmailExist(jwtTokenVerificationResult.Email)
+    if(emailChechkResult.status){
+       const userList = await getAllUserslist()
+        res.json({
+            msg : "received the request from the Amdin",
+            status : true,
+            userArray : userList
+    })
+    }else{
+        res.json({
+            msg : "Action Not Pemited ",
+            status : false
+        })
+    }
+} catch (error) {
+    
+}
 })
 module.exports = routes
